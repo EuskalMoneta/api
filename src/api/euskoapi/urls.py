085ac555b16133a83bf77d515c384d980ca34601
Florian Le Frioux <florian.lefrioux@meta-it.fr>
2016-08-17 16:45:11 +0200

    [API] Added: List all payment modes => GET /payment-modes/ (these payment modes are hardcoded)

--- src/api/euskoapi/urls.py
+++ src/api/euskoapi/urls.py
@@ -3,8 +3,9 @@
 from django.conf.urls import url
 from rest_framework import routers
 
-from members.views import MembersAPIView, MembersSubscriptionsAPIView
 from dolibarr_data.views import towns_by_zipcode, countries, country_by_id
+from euskalmoneta_data.views import payment_modes
+from members.views import MembersAPIView, MembersSubscriptionsAPIView
 
 
 router = routers.SimpleRouter()
@@ -12,6 +13,7 @@ router.register(r'members', MembersAPIView, base_name='members')
 router.register(r'members-subscriptions', MembersSubscriptionsAPIView, base_name='members-subscriptions')
 
 urlpatterns = [
+    url(r'^payment-modes/$', payment_modes),
     url(r'^towns/$', towns_by_zipcode),
     url(r'^countries/$', countries),
     url(r'^countries/(?P<id>[^/.]+)/$', country_by_id),
