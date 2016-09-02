807dc3780fb73e821437ab7dd37d8e3bff24241c
Florian Le Frioux <florian.lefrioux@meta-it.fr>
2016-09-02 14:53:26 +0200

    [WIP] Tests on CI for Euskal Moneta API (this doesn't work yet)

--- test.sh
+++ test.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
+# Test Euskal Moneta API
 
-# Test API
+COMPOSE_PROJECT_NAME='eusko_api_test'  # Avoid conflicts with non-test machines.
+export COMPOSE_PROJECT_NAME
 
-# Test Front BDC
+docker-compose rm -f
+
+set -e
+
+function run {
+    # test -d etc || cp -r etc_ci etc  # Provision local configuration.
+    docker-compose build
+
+    echo "Testing api..."
+    docker-compose up -d api
+    echo "Waiting for api to startup..."
+    sleep 30
+    docker-compose run --rm -u root api bash -c "py.test"
+}
+
+function teardown {
+    docker-compose stop
+    docker-compose rm -f
+}
+
+( run && teardown ) || ( teardown && exit 1 )
