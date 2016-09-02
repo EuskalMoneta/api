c352e5183dc054a315e92bcef86b4697610a59bd
Florian Le Frioux <florian.lefrioux@meta-it.fr>
2016-09-02 16:37:15 +0200

    [IMP] GET accounts_summaries() OK: Maybe still needs some parsing on the client-side

--- src/api/cyclos_data/views.py
+++ src/api/cyclos_data/views.py
@@ -1,10 +1,11 @@
 import logging
 
 from django.conf import settings
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from cyclos_api import CyclosAPI
+from cyclos_api import CyclosAPI, CyclosAPIException
 
 log = logging.getLogger()
 
@@ -12,18 +13,37 @@ log = logging.getLogger()
 @api_view(['GET'])
 def accounts_summaries(request):
     """
-    List all accounts_summaries.
+    List all accounts_summaries for this BDC user.
     """
-    cyclos = CyclosAPI(auth_string=request.user.profile.cyclos_auth_string)
-    # TODO steps:
-    # getCurrentUser => get ID
-    #  - user/load for this ID
-    #   - get BDC ID
-    #      - account/getAccountsSummary
-    #        results
+    try:
+        cyclos = CyclosAPI(auth_string=request.user.profile.cyclos_auth_string)
+    except CyclosAPIException:
+        Response({'Unable to connect to Cyclos!'}, status=status.HTTP_400_BAD_REQUEST)
 
-    settings.CYCLOS_CONSTANTS['cyclos_constants']['user_custom_fields']['bdc']
-    bdc_id = ''
-    query_data = [bdc_id, None]
+    # getCurrentUser => get ID for current user
+    try:
+        user_profile = cyclos.post(model='user/getCurrentUser', data=[])
+        user_id = user_profile['result']['id']
+    except CyclosAPIException:
+        Response({'Unable to connect to Cyclos!'}, status=status.HTTP_400_BAD_REQUEST)
+    except KeyError:
+        Response({'Unable to fetch Cyclos data! Maybe your credentials are invalid!?'},
+                 status=status.HTTP_400_BAD_REQUEST)
+
+    # user/load for this ID to get field BDC ID
+    try:
+        user_data = cyclos.post(model='user/load', data=user_id)
+        user_bdc_id = [item['linkedEntityValue']['id']
+                       for item in user_data['result']['customValues']
+                       if item['field']['id'] ==
+                       str(settings.CYCLOS_CONSTANTS['cyclos_constants']['user_custom_fields']['bdc'])][0]
+    except CyclosAPIException:
+        Response({'Unable to connect to Cyclos!'}, status=status.HTTP_400_BAD_REQUEST)
+    except (KeyError, IndexError):
+        Response({'Unable to fetch Cyclos data! Maybe your credentials are invalid!?'},
+                 status=status.HTTP_400_BAD_REQUEST)
+
+    # account/getAccountsSummary
+    query_data = [user_bdc_id, None]
     results = cyclos.post(model='account/getAccountsSummary', data=query_data)
     return Response(results)
