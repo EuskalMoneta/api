dc1b0f0e6378fc6e64c6abebcbdc44a261baf8f5
Florian Le Frioux <florian.lefrioux@meta-it.fr>
2016-09-16 15:24:36 +0200

    [IMP] Adding comments + renaming script to setup_cyclos.sh

---
+++ etc/cyclos/setup_cyclos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This is how I launch this script (in dev):
+# docker-compose exec api bash /cyclos/setup_cyclos.sh
+
+# This cd will do this: cd /cyclos/
+cd "${0%/*}"
+
+rm -f cyclos_constants.yml
+
+# Base64('admin:admin') = YWRtaW46YWRtaW4=
+python setup.py http://cyclos-app:8080/ YWRtaW46YWRtaW4=
+python init_static_data.py http://cyclos-app:8080/ YWRtaW46YWRtaW4=
--- etc/cyclos/setup_cyclos_dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "${0%/*}"
-
-rm -f cyclos_constants.yml
-
-python setup.py http://cyclos-app:8080/ YWRtaW46YWRtaW4=
-python init_static_data.py http://cyclos-app:8080/ YWRtaW46YWRtaW4=
