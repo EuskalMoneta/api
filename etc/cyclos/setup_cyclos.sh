93fe960b02ad2beab89a1692c04869de1074d98a
Guillaume Masson <guillaume.masson@meta-it.fr>
2016-10-31 15:34:54 +0100

    [IMP] Homogenous behaviour in dev and other flavours (no onbuild, api wait for cyclos to be fully up)

--- etc/cyclos/setup_cyclos.sh
+++ etc/cyclos/setup_cyclos.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
+set -x
+set -e
+
+cd /cyclos
+
+PASS=`echo -n admin:admin | base64`
+
+until [ `curl --silent --write-out '%{response_code}' -o /dev/null http://cyclos-app:8080/global/` -eq 200 ];
+do
+  echo '--- waiting for Cyclos to be fully up (10 seconds)'
+  sleep 10
+done
+
+if [ ! -f ./cyclos_constants.yml ]; then
+    python setup.py http://cyclos-app:8080/ $PASS
+    python init_static_data.py http://cyclos-app:8080/ $PASS
+fi
+
+exec "$@"
+
 
 # This is how I launch this script (in dev):
 # docker-compose exec api bash /cyclos/setup_cyclos.sh
 
 # This cd will do this: cd /cyclos/
-cd "${0%/*}"
-
-rm -f cyclos_constants.yml
-
-# Base64('admin:admin') = YWRtaW46YWRtaW4=
-python setup.py http://cyclos-app:8080/ YWRtaW46YWRtaW4=
-python init_test_data.py http://cyclos-app:8080/ YWRtaW46YWRtaW4=
+#~ cd "${0%/*}"
+#~ 
+#~ echo $PWD
+#~ 
+#~ rm -f cyclos_constants.yml
+#~ 
+#~ # Base64('admin:admin') = YWRtaW46YWRtaW4=
+#~ python setup.py http://cyclos-app:8080/ YWRtaW46YWRtaW4=
+#~ python init_static_data.py http://cyclos-app:8080/ YWRtaW46YWRtaW4=
