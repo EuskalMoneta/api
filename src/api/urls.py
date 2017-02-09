f4f5288f2a91ddcac2cf923249360169fa81801f
Martineau <a.martineau@net.estia.fr>
2017-02-09 17:28:06 +0100

    [FIX] Export to csv into commentary (not done yet)

--- src/api/urls.py
+++ src/api/urls.py
@@ -86,6 +86,7 @@ urlpatterns = [
     url(r'^payments-available-history-adherent/$', cel_views.payments_available_for_adherents),
     url(r'^account-summary-adherents/$', cel_views.account_summary_for_adherents),
     url(r'^export-history-adherent-pdf/$', cel_views.export_history_adherent_pdf),
+    #url(r'^export-history-adherent-csv/$', cel_views.export_history_adherent_csv),
 ]
 
 urlpatterns += router.urls
