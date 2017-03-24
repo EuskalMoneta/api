bdcae5fa169e23fcdddd9e32b51c7d73b18b6078
Xebax <xebax@wanadoo.fr>
2017-03-24 13:11:22 +0100

    [WIP] Add the parameters to the GET /prestataires/ endpoint.

--- src/api/prestataires/views.py
+++ src/api/prestataires/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 from rest_framework.response import Response
 
 from base_api import BaseAPIView
-from dolibarr_api import DolibarrAPIException
+from dolibarr_api import DolibarrAPI, DolibarrAPIException
 from prestataires import serializers
 from pagination import CustomPagination
 
@@ -18,6 +18,33 @@ class PrestatairesAPIView(BaseAPIView):
         super(PrestatairesAPIView, self).__init__()
 
     def list(self, request):
+        """
+        Récupérer la liste des prestataires.
+        """
+        language = request.GET.get('langue')
+        keyword = request.GET.get('mot-cle')
+        category_id = request.GET.get('categorie')
+        town_id = request.GET.get('ville')
+        zipcode = request.GET.get('code-postal')
+        bdc = request.GET.get('bdc', False)
+        euskokart = request.GET.get('euskokart', False)
+
+        logger.debug('language=' + str(language))
+        logger.debug('keyword=' + str(keyword))
+        logger.debug('category_id=' + str(category_id))
+        logger.debug('town_id=' + str(town_id))
+        logger.debug('zipcode=' + str(zipcode))
+        logger.debug('bdc=' + str(bdc))
+        logger.debug('euskokart=' + str(euskokart))
+
+        if not language:
+            return Response({'error': 'The "langue" parameter is required.'}, status=status.HTTP_400_BAD_REQUEST)
+        if language not in ('eu', 'fr'):
+            return Response({'error': 'Invalid value for the "langue" parameter.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        dolibarr = DolibarrAPI(api_key=request.user.profile.dolibarr_token)
+#        return Response(dolibarr.get(model='towns', zipcode=search))
+
         objects = []
         paginator = CustomPagination()
         result_page = paginator.paginate_queryset(objects, request)
