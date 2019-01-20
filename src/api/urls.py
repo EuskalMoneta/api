875535bb4154b4782fba7f96ce3c1c70a18e452e
Xebax <xebax@wanadoo.fr>
2019-01-20 18:47:39 +0100

    [CLE] Suppression des fonctions pour l'annuaire des prestataires.
    Cette partie de l'API n'a jamais servi en production et n'est plus à
    jour suite aux changements apportés au paramétrage de Dolibarr.

--- src/api/urls.py
+++ src/api/urls.py
@@ -7,7 +7,6 @@ from bureauxdechange.views import BDCAPIView
 from members.views import MembersAPIView, MembersSubscriptionsAPIView
 from cel.beneficiaire import BeneficiaireViewSet
 from cel.security_qa import SecurityQAViewSet
-from prestataires.views import AnnuairePrestatairesAPIView, CategoriesPrestatairesAPIView, VillesPrestatairesAPIView
 
 from auth_token import views as auth_token_views
 import bdc_cyclos.views as bdc_cyclos_views
@@ -24,9 +23,6 @@ router.register(r'members', MembersAPIView, base_name='members')
 router.register(r'members-subscriptions', MembersSubscriptionsAPIView, base_name='members-subscriptions')
 router.register(r'beneficiaires', BeneficiaireViewSet, base_name='beneficiaires')
 router.register(r'securityqa', SecurityQAViewSet, base_name='securityqa')
-router.register(r'annuaire-prestataires', AnnuairePrestatairesAPIView, base_name='annuaire-prestataires')
-router.register(r'categories-prestataires', CategoriesPrestatairesAPIView, base_name='categories-prestataires')
-router.register(r'villes-prestataires', VillesPrestatairesAPIView, base_name='villes-prestataires')
 
 urlpatterns = [
     # Auth token
