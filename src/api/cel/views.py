4d2c1b9b9141badc269d3ea19ea341a7945565a9
Xabi <xabi@euskalmoneta.org>
2025-11-17 14:59:48 +0100

    Suppression 2 lignes paraistes en fin de fichier

--- src/api/cel/views.py
+++ src/api/cel/views.py
@@ -1633,6 +1633,3 @@ def batch_add_mandats(request):
         'data': filtered_members,
         'count': len(filtered_members)
     }, status=status.HTTP_200_OK)
-
-@api_view(['POST'])
-@permission_classes((AllowAny, ))
